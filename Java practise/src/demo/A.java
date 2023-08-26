f3d8fb85852c61d738b0f85efb8d3e88cd35676f
Lidiya <Lidiya@LAPTOP-1VL2SLMT>
2023-08-26 11:56:20 +0530

    This is java program

---
+++ Java practise/src/demo/A.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class A {
+	static int a=10;
+	public static void m1()
+	{
+		System.out.println("it is a static method");
+	}
+public static void main(String[]args)
+{
+	System.out.println("the static variable "+a);
+	m1();
+}
+}
